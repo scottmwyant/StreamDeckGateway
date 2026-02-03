945ecf216621464408541cef67f2bf75ffcbcb43
Scott Wyant <29161635+scottmwyant@users.noreply.github.com>
2026-02-02 23:04:06 -0500

    Script to build streamdeck.tar.gz

---
+++ scripts/pack.ps1
@@ -0,0 +1,32 @@
+#
+# Call this script from the root of the project
+#
+
+$tmp = "streamdeck"      # Staging directory
+$pub = "streamdeck.tar.gz"  # .tar archive file for distribution
+
+# Clean artifacts
+if(Test-Path $tmp) {
+    Remove-Item -Path $tmp -Recurse -Force
+}
+if(Test-Path $pub) {
+    Remove-item $pub -Force
+}
+
+# Copy source code from external dependencies
+Copy-Item -Path "paho\src" -Destination $tmp -Recurse 
+Copy-Item -Path "pyhidapi\hid\" -Destination $tmp -Recurse 
+
+# Copy source files from this project
+Copy-Item -Path "src\*" -Exclude "test" -Destination $tmp
+
+# Pack
+& tar.exe -czf $pub $tmp
+
+# Remove the staging directory
+Remove-Item -Path $tmp -Recurse -Force
+
+# List the content of the tarball we just created
+Write-Host "`n$pub contains the following:`n"
+& tar.exe -tf $pub
+Write-Host ""
